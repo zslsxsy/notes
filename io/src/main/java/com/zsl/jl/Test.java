d375fccffc7c6d47785dbeb74682c8bf711ecbc3
ZhangShulin <zhangshulin@juzifenqi.com>
2021-01-30 14:18:53 +0800

    测试ignore

---
+++ io/src/main/java/com/zsl/jl/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Juzifenqi.com Inc.
+ * Copyright (c) 2019-2029 All Rights Reserved.
+ */
+package com.zsl.jl;
+
+/**
+ * @author ZhangShulin
+ * @date 2021/1/30
+ */
+public class Test {
+}
